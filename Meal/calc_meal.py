16718b134c499b6b661be09594165c44dbc6504d
Md. Gofran Khan <gofran0123@gmail.com>
2019-02-01 22:24:06 +0600

    all calculation added for meal system

--- Meal/calc_meal.py
+++ Meal/calc_meal.py
@@ -23,38 +23,6 @@ def main():
 				default=0,
 				type=int,
 				help='Store pani bill which distributed amoung 5.')
-	parser.add_argument('-s', action='store',
-                    dest='simple_value',
-                    help='Store a simple value')
-
-	parser.add_argument('-c', action='store_const',
-		            dest='constant_value',
-		            const='value-to-store',
-		            help='Store a constant value')
-
-	parser.add_argument('-t', action='store_true',
-		            default=False,
-		            dest='boolean_t',
-		            help='Set a switch to true')
-	parser.add_argument('-f', action='store_false',
-		            default=True,
-		            dest='boolean_f',
-		            help='Set a switch to false')
-
-	parser.add_argument('-a', action='append',
-		            dest='collection',
-		            default=[],
-		            help='Add repeated values to a list')
-
-	parser.add_argument('-A', action='append_const',
-		            dest='const_collection',
-		            const='value-1-to-append',
-		            default=[],
-		            help='Add different values to list')
-	parser.add_argument('-B', action='append_const',
-		            dest='const_collection',
-		            const='value-2-to-append',
-		            help='Add different values to list')
 
 	parser.add_argument('--version', action='version',
 		            version='%(prog)s 1.0')
@@ -75,19 +43,37 @@ def main():
 	for x in Bazar:
 		total_bazar += x
 
+	meal_rate =  total_bazar/total_meal
+	mealCost = []
+	for x in range(0, len(names)):
+		mealCost.append(int(mealCount[x]*meal_rate))
+
 	results = parser.parse_args()
-	print('House Rent          = {!r}'.format(results.rent))
+	print('House Rent          = {!r}'.format(results.rent)) 
 	print('Bua Bill            = {!r}'.format(results.bua))
 	print('Net Bill            = {!r}'.format(results.net))
 	print('Pani Bill           = {!r}'.format(results.pani))
 	print('Total Meal          = {!r}'.format(total_meal))
 	print('Bazar               = {!r}'.format(total_bazar))
-	print('simple_value        = {!r}'.format(results.simple_value))
-	print('constant_value      = {!r}'.format(results.constant_value))
-	print('boolean_t           = {!r}'.format(results.boolean_t))
-	print('boolean_f           = {!r}'.format(results.boolean_f))
-	print('collection          = {!r}'.format(results.collection))
-	print('const_collection    = {!r}'.format(results.const_collection))
+	print('Meal Rate           = {!r}'.format(meal_rate))
+
+	payable = []
+	i = 0
+	for name in names:
+		payable.append(int (results.rent/5 + results.net/5 + results.bua/5 + results.pani/5 + mealCost[i]))
+		i = i + 1
+
+	print('-'* 140)
+	print("| Name          |  House Rent |   Net Bill |   Bua Bill   |   Pani Bill |   Meal Cost   |   Total Payable |  Total Bazar |  Settlement     |")
+	index = 0
+	for name in names:
+		print('-'* 140)
+		print('| %s    |     %d    |     %d    |     %d      |      %d      |     %d       |      %d       |     %d      |        %d     |' 
+		%(names[index] + " "*(10-len(names[index])), results.rent/5, results.net/5, results.bua/5, results.pani/5, mealCost[index], 
+		payable[index], Bazar[index], payable[index]-Bazar[index]))
+		index = index + 1
+	print('-'* 140)
+
 
 if __name__=="__main__":
 	sys.exit(main())
